259c6ddfe0f77516a785ef8ac451f8d54236179a
Lucas Cavalcante <lcdaponte@gmail.com>
2023-04-14 22:48:28 -0300

    initial setup

---
+++ src/main/java/com/lcdaponte/awsrekognition/AwsRekognitionApplication.java
@@ -0,0 +1,13 @@
+package com.lcdaponte.awsrekognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsRekognitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsRekognitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lcdaponte/awsrekognition/AwsRekognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcdaponte.awsrekognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsRekognitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
